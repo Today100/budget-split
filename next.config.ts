e27ea3f9682ae9b5709a2cb2fc47c03dcb706424
Grace Zhang <ruoshui.grace.zhang@gmail.com>
2026-09-07 12:55:31 -0400

    fix authentication

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,15 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        // Intercepts auth traffic on your Vercel domain
         source: '/__/auth/:path*',
-        // Proxies it to your actual Firebase project
         destination: 'https://budget-25294.firebaseapp.com/__/auth/:path*',
       },
+      {
+        source: '/__/firebase/:path*',
+        destination: 'https://budget-25294.firebaseapp.com/__/firebase/:path*',
+      }
     ];
   },
 };
 
-export default nextConfig; // Use module.exports = nextConfig; if using .js instead of .mjs
+export default nextConfig;
